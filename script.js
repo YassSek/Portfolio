0dc3249d81e382149cb7f012ff3b133dcb16abf3
MiniYass <sekri.yassine@gmail.com>
2021-05-14 18:12:58 +0200

    remplacement de certain fichier

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const contactBtn = document.getElementById('contact');
 const aboutBtn = document.getElementById('about');
-
 const blocAbout = document.getElementById('blocAbout');
 const blocContact = document.getElementById('blocContact');
 
 aboutBtn.addEventListener('click', () =>{
-
     if(blocAbout.style.display=="block"){
         blocAbout.style.display="none";
     }else{
@@ -13,15 +11,23 @@ aboutBtn.addEventListener('click', () =>{
         blocAbout.style.display="block";
     }
 
+   document.addEventListener('scroll',()=>{
+        if(window.scrollY>50){
+            blocAbout.style.display="none";  
+        } 
+    })
 })
 
 contactBtn.addEventListener('click', () =>{
-    
     if(blocContact.style.display=="block"){
         blocContact.style.display="none";
     }else{
         blocContact.classList.add('slideContact')
         blocContact.style.display="block";
     }
-    
+    document.addEventListener('scroll',()=>{
+        if(window.scrollY>50){
+            blocContact.style.display="none";
+        } 
+    })
 })
