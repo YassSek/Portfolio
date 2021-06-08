a0ae1056c5ea3f6fcf2e804f0a3d66361c629ec7
MiniYass <sekri.yassine@gmail.com>
2021-06-08 12:57:10 +0200

    adding favicon , changing colors , responsive OK text NOK

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const aboutBtn = document.getElementById('about');
 const blocAbout = document.getElementById('blocAbout');
 const blocContact = document.getElementById('blocContact');
 
-aboutBtn.addEventListener('click', () =>{
-    if(blocAbout.style.display=="block"){
-        blocAbout.style.display="none";
-    }else{
-        blocAbout.classList.add('slideAbout'); 
-        blocAbout.style.display="block";
+aboutBtn.addEventListener('click', () => {
+    if (blocAbout.style.display == "block") {
+        blocAbout.style.display = "none";
+    } else {
+        blocAbout.classList.add('slideAbout');
+        blocAbout.style.display = "block";
     }
-   document.addEventListener('scroll',()=>{
-        if(window.scrollY>50){
-            blocAbout.style.display="none";  
-        } 
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            blocAbout.style.display = "none";
+        }
     })
 })
 
-contactBtn.addEventListener('click', () =>{
-    if(blocContact.style.display=="block"){
-        blocContact.style.display="none";
-    }else{
+contactBtn.addEventListener('click', () => {
+    if (blocContact.style.display == "block") {
+        blocContact.style.display = "none";
+    } else {
         blocContact.classList.add('slideContact')
-        blocContact.style.display="block";
+        blocContact.style.display = "block";
     }
-    document.addEventListener('scroll',()=>{
-        if(window.scrollY>40){
-            blocContact.style.display="none";
-        } 
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > 40) {
+            blocContact.style.display = "none";
+        }
     })
 })
